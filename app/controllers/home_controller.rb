cbdba5c55427401ca6096e773b73121f8140637b
Beta-23 <alfred.dominguez@gmail.com>
2019-10-24 12:00:12 -0700

    Implement JSON data request via home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
+
+    require 'net/http'
+    require 'json'
+
+  # instance method to fetch JSON data from coinmarketcap and parse it
+  
   def index
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
   end
 
   def about
