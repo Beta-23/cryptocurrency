8a8906e60da9c6eb7ed1e6f1f1b3aeba4438db05
Beta-23 <alfred.dominguez@gmail.com>
2019-10-26 00:21:38 -0700

    Added error handling for search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,8 @@
 class HomeController < ApplicationController
-
     require 'net/http'
     require 'json'
-
+    
   # instance method to fetch JSON data from coinmarketcap and parse it
-
   def index
     @url = 'https://api.coinmarketcap.com/v1/ticker/'
     @uri = URI(@url)
@@ -18,6 +16,15 @@ class HomeController < ApplicationController
   end
 
   def search
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+   # converts response to a Ruby hash 
+    @search_coin = JSON.parse(@response)
     @symbol = params[:sym]
+
+    if @symbol
+      @symbol = @symbol.upcase
+    end
   end
 end
