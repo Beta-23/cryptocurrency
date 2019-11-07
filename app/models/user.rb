8922cc1b13d5d311c440dd8da4f5b5cca68a3755
Beta-23 <alfred.dominguez@gmail.com>
2019-11-07 13:22:29 -0800

    Implemented user email validations to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  #this method converts the user email in lowercase before hittig the database
+  before_save { self.email = email.downcase }
+  
+  # vlaidates database email, checks for unique email, checks case sensitive email and max require.  
+  # REGEX- validates email address via regular expressions.
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, 
+            length: { maximum: 50 },
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
 end
